80ee8cde4d769fda76370b4c174cca9dfebf57b7
leilin1995 <leilin@cug.edu.cn>
2022-06-22 17:06:12 +0800

    submit

---
+++ setup.py
@@ -0,0 +1,21 @@
+"""
+__author__ = 'linlei'
+__project__:setup.py
+__time__:2022/6/22 15:29
+__email__:"919711601@qq.com"
+"""
+import setuptools
+
+setuptools.setup(
+    name="CommonCode_LeiLin",
+    version="0.0.1",
+    author="leilin",
+    author_email="919711601@qq.com",
+    description="This is the code base that Lei Lin commonly uses in his research.",
+    packages=setuptools.find_packages(),
+    url="https://github.com/leilin1995/CommonCode",
+    classifiers=["Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+                         ],
+)
